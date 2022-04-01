c74022f192cad78b803f78cc8e8770fc7b9f1c2f
Proioxis4 <lucadaniele444@gmail.com>
2022-04-01 18:03:49 +0200

    about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 
-import { Row, Col } from "antd";
+import { Row, Col, Typography } from "antd";
 import "../styles/About.css";
+const { Title, Text } = Typography;
 const About = () => {
   const items = [
     {
       key: "1",
       icon: <i className="fas fa-chart-pie"></i>,
       title: "Crypto News",
-      content:
-        "cu nostro dissentias consectetuer mel. Ut admodum conceptam mei, cu eam tation fabulas abhorreant. His ex mandamus.",
+      content: "Stay always tuned on the cryptoverse thanks to our news feed.",
     },
     {
       key: "2",
       icon: <i className="fas fa-desktop"></i>,
       title: "Trade",
-      content:
-        "cu nostro dissentias consectetuer mel. Ut admodum conceptam mei, cu eam tation fabulas abhorreant. His ex mandamus.",
+      content: "Register and start to practice trading for free!",
     },
     {
       key: "3",
       icon: <i className="fas fa-database"></i>,
-      title: "Charts",
+      title: "up-to-date Live Trading Charts",
       content:
-        "cu nostro dissentias consectetuer mel. Ut admodum conceptam mei, cu eam tation fabulas abhorreant. His ex mandamus.",
+        "Thanks to trading view you can make the plans that best suit your trading strategy.",
     },
   ];
   return (
@@ -56,7 +55,8 @@ const About = () => {
           })}
         </Row>
         <Title>About Us</Title>
-        yada yada
+        This website is made entirely by Daniele Luca, reach me through github
+        or linkedin. --bottone github--bottone linkedin
       </div>
     </div>
   );
